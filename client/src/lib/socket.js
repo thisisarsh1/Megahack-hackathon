eee55dfee287adb9242c04a3f073ff9c762f3a02
Nitin Gupta <nitin524gupta@eng.rizvi.edu.in>
2025-03-08 09:12:24 +0530

    small change

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -5,9 +5,10 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:8080", {
   transports: ["websocket"],
   cors: {
-    origin: "http://localhost:3000", // Your frontend URL
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
+
 
 export default socket;
